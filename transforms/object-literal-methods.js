0fedc5f1e9c4b12778ca3782f6c4c1eaa9a410f6
Remon Oldenbeuving <remon@hackerone.com>
2015-12-15 17:46:16 +0100

    Added Object literal method transform

---
+++ transforms/object-literal-methods.js
@@ -0,0 +1,51 @@
+/*
+Convert object methods to ES2015 object literal methods.
+
+Example:
+
+```coffeescript
+method: (args) ->
+  console.info(args)
+```
+
+```javascript
+({
+  method(args) {
+    return console.info(args);
+  }
+});
+```
+*/
+
+export default (file, api) => {
+  const j = api.jscodeshift;
+  const root = j(file.source);
+
+  const objectLiteralsChanged = root
+    .find(j.Property, {
+      value: {
+        type: "FunctionExpression",
+      },
+    })
+    .replaceWith(exp => {
+      const prop = j.property(
+        exp.node.kind,
+        exp.node.key,
+        j.functionExpression(
+          null,
+          exp.node.value.params,
+          exp.node.value.body
+        )
+      );
+
+      prop.method = true;
+      return prop;
+    })
+    .size() > 0;
+
+  if (objectLiteralsChanged) {
+    return root.toSource();
+  }
+
+  return null;
+};
