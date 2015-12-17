feb09631faeb3a464fe8c162045f64545856f12a
Remon Oldenbeuving <remon@hackerone.com>
2015-12-17 15:44:40 +0100

    Cleaned up and tightened for-in-loop transform

--- transforms/for-in-loops.js
+++ transforms/for-in-loops.js
@@ -13,19 +13,9 @@ for (a of b) {
   return console.info(a);
 }
 ```
-
-Assumptions:
-- All for-loops that have an update and where the first expression in the body
-  is an UpdateExpression are considered CoffeeScript for..in loops and therefor converted
-- If the update of a for loop is an assignment, we assume that we need to keep track
-  of index, and therfor need to run `.forEach`
-
-FIXME:
-- This transform is pretty big on the assumptions, we should try and lock it down
-  more.
-- This transform doesn't clean up after itself, it oftentimes leaves `ref`, `len`
-  variable declarations in the body.
 */
+
+import removeVariableDeclarator from "./util/remove-variable-declarator";
 
 module.exports = (file, api) => {
   const j = api.jscodeshift;
@@ -33,18 +23,74 @@ module.exports = (file, api) => {
 
   const forStatementsChanged = root
     .find(j.ForStatement, {
+      init: {
+        type: "SequenceExpression",
+        expressions: [
+          {
+            type: "AssignmentExpression",
+            operator: "=",
+            left: {
+              type: "Identifier",
+            },
+            right: {
+              type: "Literal",
+              value: 0,
+            },
+          }, {
+            type: "AssignmentExpression",
+            operator: "=",
+            left: {
+              type: "Identifier",
+            },
+            right: {
+              type: "MemberExpression",
+              property: {
+                type: "Identifier",
+                name: "length",
+              },
+            },
+          },
+        ],
+      },
+      test: {
+        type: "BinaryExpression",
+        operator: "<",
+        left: {
+          type: "Identifier",
+        },
+        right: {
+          type: "Identifier",
+        },
+      },
       update: {
         type: "UpdateExpression",
+        prefix: false,
+        operator: "++",
       },
     })
-    .filter(exp => exp.value.body.body[0].type === "ExpressionStatement")
+    .filter(exp => {
+      const firstBodyLine = exp.value.body.body[0];
+      const initExpressions = exp.value.init.expressions;
+
+      return firstBodyLine.type === "ExpressionStatement" &&
+        // Match the `i` in `b[i]` with `i = 0`
+        firstBodyLine.expression.right.property.name === initExpressions[0].left.name &&
+        // Match the `b` in `b[i]` with `b.length`
+        firstBodyLine.expression.right.object.name === initExpressions[1].right.object.name;
+    })
     .replaceWith(exp => {
       const oldBody = exp.value.body;
-      const key = oldBody.body[0].expression.left.name;
-      const left = j.variableDeclaration("let", [j.identifier(key)]);
-      const newBody = exp.value.body;
-      const oldKeyExpression = newBody.body.shift().expression;
+      const oldKeyExpression = oldBody.body.shift().expression;
+      const valueVariableName = oldKeyExpression.left.name;
       const right = oldKeyExpression.right.object;
+      const left = j.variableDeclaration("let", [j.identifier(valueVariableName)]);
+
+      // Will be overwritten by `let`
+      removeVariableDeclarator(valueVariableName, exp.scope.path, api);
+      // internal variable that keeps track of the index
+      removeVariableDeclarator(oldKeyExpression.right.property.name, exp.scope.path, api);
+      // internal variable that keeps track of the length of the array that's being looped
+      removeVariableDeclarator(exp.value.init.expressions[1].left.name, exp.scope.path, api);
 
       return j.forOfStatement(left, right, exp.value.body);
     })
