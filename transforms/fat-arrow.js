50ea486c49ceb4246429c8d94de2493086c5fc18
Remon Oldenbeuving <remon@hackerone.com>
2015-12-29 14:58:52 +0100

    Refactored the fat-arrow transform
    This transform now also transforms nested fat-arrow methods.

--- transforms/fat-arrow.js
+++ transforms/fat-arrow.js
@@ -18,40 +18,77 @@ Assumptions:
 - Every time that you have a CallExpression that has a FunctionExpression as a
   callee and `this` as only argument, we assume that this is in fact a
   CoffeeScript fat arrow method.
-
-FIXME:
-- This transform should be checked against the CoffeeScript source to see if the
-  assumptions are correct.
 */
 
 module.exports = (file, api) => {
   const j = api.jscodeshift;
   const root = j(file.source);
 
-  const fatArrowsTransformed = root
-    .find(j.CallExpression, {
-      callee: {
-        type: "FunctionExpression",
-      },
-      arguments: [
-        { type: "ThisExpression" },
-      ],
-    })
-    .forEach(exp => {
-      const thisValue = exp.node.callee.params[0].name;
+  const transformSimpleFatArrows = () =>
+    root
+      .find(j.CallExpression, {
+        callee: {
+          type: "FunctionExpression",
+        },
+        arguments: [
+          { type: "ThisExpression" },
+        ],
+      })
+      .forEach(exp => {
+        const realMethod = exp.node.callee.body.body[0].argument;
 
-      j(exp.node.callee.body)
-        .find(j.Identifier, { name: thisValue })
-        .replaceWith(j.identifier("this"));
-    })
-    .replaceWith(exp => {
-      const realMethod = exp.node.callee.body.body[0].argument;
+        exp.replace(
+          j.arrowFunctionExpression(realMethod.params, realMethod.body)
+        );
+      })
+      .size() > 0;
 
-      return j.arrowFunctionExpression(realMethod.params, realMethod.body);
-    })
-    .size() > 0;
+  const getClosestMethodExpression = path => {
+    let currentPath = path;
 
-  if (fatArrowsTransformed) {
+    while (
+      currentPath &&
+      !(
+        j.FunctionExpression.check(currentPath.value) ||
+        j.ArrowFunctionExpression.check(currentPath.value)
+      )
+    ) {
+      currentPath = currentPath.parent;
+    }
+
+    return currentPath;
+  };
+
+  const transformIntoArrowFunction = exp =>
+    j.arrowFunctionExpression(exp.value.params, exp.value.body);
+
+  const transformNestedFatArrows = () =>
+    root
+      .find(j.FunctionExpression)
+      .filter(exp =>
+        j(exp)
+          .find(j.Identifier, { name: "_this" })
+          .filter(identifier => identifier.scope.path === exp)
+          .size() > 0
+      )
+      .forEach(exp => {
+        let method = exp;
+
+        while (method && method.value.type === "FunctionExpression") {
+          j(method).replaceWith(transformIntoArrowFunction);
+          method = getClosestMethodExpression(method.parent);
+        }
+      });
+
+  const replaceOldThis = () =>
+    root
+      .find(j.Identifier, { name: "_this" })
+      .replaceWith(j.identifier("this"));
+
+  if (transformSimpleFatArrows()) {
+    transformNestedFatArrows();
+    replaceOldThis();
+
     return root.toSource();
   }
 
