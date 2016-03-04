87a4c1cf34c4a9787ad7eb5c8236f97e3069f5f1
Remon Oldenbeuving <remon@hackerone.com>
2016-03-04 15:13:29 +0100

    Rewrote the template-strings transform

--- transforms/template-strings.js
+++ transforms/template-strings.js
@@ -12,71 +12,82 @@ Example:
 ```
 */
 
-const transformExpressionsIntoTemplates = (root, j) =>
-  root
-    .find(j.BinaryExpression, { operator: "+" })
-    .filter(exp => {
-      const left = exp.value.left;
-      const right = exp.value.right;
-
-      const stringRight = right.type === "Literal" && typeof right.value === "string";
-      const stringLeft = left.type === "Literal" && typeof left.value === "string";
-      const binaryExpressionLeft = left.type === "BinaryExpression";
-
-      const involvesStrings = stringLeft || stringRight && !binaryExpressionLeft;
-      const involvesTemplates = exp.value.left.type === "TemplateLiteral" ||
-        exp.value.right.type === "TemplateLiteral";
-
-      return involvesStrings || involvesTemplates;
-    })
-    .replaceWith(exp => {
-      const quasisFromLiteral = literal =>
-        j.templateElement({ cooked: literal.value, raw: literal.rawValue }, false);
-      const left = exp.value.left;
-      const right = exp.value.right;
-      const stringRight = right.type === "Literal" && typeof right.value === "string";
-      const stringLeft = left.type === "Literal" && typeof left.value === "string";
-
-      const quasis = [];
-      const expressions = [];
-
-      if (stringLeft) {
-        quasis.push(quasisFromLiteral(left));
-      } else if (left.type === "TemplateLiteral") {
-        quasis.push(...left.quasis);
-        expressions.push(...left.expressions);
-      } else {
-        quasis.push(j.templateElement({ cooked: "", raw: "" }, false));
-        expressions.push(left);
-      }
-
-      if (stringRight) {
-        quasis.push(quasisFromLiteral(right));
-      } else if (right.type === "TemplateLiteral") {
-        quasis.push(...right.quasis);
-        expressions.push(...right.expressions);
-      } else {
-        if (quasis.length === 0) {
-          quasis.push(j.templateElement({ cooked: "", raw: "" }, false));
-        }
-
-        expressions.push(right);
-        quasis.push(j.templateElement({ cooked: "", raw: "" }, false));
-      }
-
-      return j.templateLiteral(quasis, expressions);
-    })
-    .size() > 0;
-
 module.exports = (file, api) => {
   const j = api.jscodeshift;
   const root = j(file.source);
+
+  const appendNodeToTemplate = (node, template) => {
+    var quasis = template.quasis;
+    var expressions = template.expressions;
+    var lastQuasisValue = quasis[quasis.length - 1].value;
+
+    switch (node.type) {
+    case "TemplateLiteral":
+      var [firstRightQuasis, ...restRightQuasis] = node.quasis;
+      quasis[quasis.length - 1] = j.templateElement({
+        cooked: lastQuasisValue.cooked + firstRightQuasis.value.cooked,
+        raw: lastQuasisValue.raw + firstRightQuasis.value.raw,
+      }, firstRightQuasis.tail);
+      quasis.push(...restRightQuasis);
+      expressions.push(...node.expressions);
+      break;
+    case "Literal":
+      quasis[quasis.length - 1] = j.templateElement({
+        cooked: lastQuasisValue.cooked + node.value,
+        raw: lastQuasisValue.raw + node.rawValue,
+      }, true);
+      break;
+    default:
+      quasis[quasis.length - 1].tail = false;
+      expressions.push(node);
+      quasis.push(j.templateElement({
+        cooked: "",
+        raw: "",
+      }, true));
+      break;
+    }
+  };
+
+  const transformBinaryExpressions = () =>
+    root
+      .find(j.BinaryExpression, { operator: "+" })
+      .filter(exp => {
+        const left = exp.value.left;
+        const right = exp.value.right;
+        const leftType = left.type;
+        const rightType = right.type;
+
+        // We'll convert these nested expressions inside out, not outside in
+        if (leftType === "BinaryExpression" || rightType === "BinaryExpression") {
+          return false;
+        }
+
+        const leftIsString = leftType === "Literal" && typeof left.value === "string";
+        const rightIsString = rightType === "Literal" && typeof right.value === "string";
+        const leftIsTemplate = leftType === "TemplateLiteral";
+        const rightIsTemplate = rightType === "TemplateLiteral";
+
+        return leftIsString || rightIsString || leftIsTemplate || rightIsTemplate;
+      })
+      .replaceWith(exp => {
+        const template = j.templateLiteral(
+          [j.templateElement({ cooked: "", raw: "" }, true)],
+          []
+        );
+
+        appendNodeToTemplate(exp.value.left, template);
+        appendNodeToTemplate(exp.value.right, template);
+
+        return template;
+      })
+      .size() > 0;
+
   let expressionsChanged = true;
   let runCounter = 0;
 
   while (expressionsChanged) {
     runCounter++;
-    if (!transformExpressionsIntoTemplates(root, j)) {
+    if (!transformBinaryExpressions()) {
       expressionsChanged = false;
     }
   }
